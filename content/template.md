4247e1e8aa12b890d14a60200d919340ddcc5e5c
George <george.levines@gmail.com>
2016-05-01 02:39:07 -0400

    md template and .conf changes

---
+++ content/template.md
@@ -0,0 +1,9 @@
+Status: draft
+Title: Hello world!
+Date: 2016-04-29 00:32
+Category: Hello world!
+Tags: first post
+Slug: hello-world
+Authors: George LeVines
+Summary: A nice 'Hello world!' post that tests a number of formats
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,11 +4,14 @@ from __future__ import unicode_literals
 
 AUTHOR = u'George'
 SITENAME = u'by George'
-SITEURL = ''
+SITEURL = 'http://bygeorge.xyz'
+TIMEZONE = 'America/New_York'
 
 PATH = 'content'
+DEFAULT_DATE_FORMAT = '%a %d %B %Y'
 
-TIMEZONE = 'America/New_York'
+# can be useful in development, but set to False when you're ready to publish
+RELATIVE_URLS = True
 
 DEFAULT_LANG = u'en'
 
@@ -33,6 +36,9 @@ DEFAULT_PAGINATION = 1
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Content Cache (Off for dev/On for live)
+LOAD_CONTENT_CACHE = False
 
 THEME = "themes/bygeorge"
 CSS_FILE = 'style.css'
